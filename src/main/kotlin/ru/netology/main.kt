ad2a835593db849ce9c35d9e11ffa6787c64a07f
Alina Prokofeva <whitestring@inbox.ru>
2022-02-08 02:45:25 +0300

    checked fun checkDiscount

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,8 +1,8 @@
 package ru.netology
 
 fun main() {
-    val isRegularCustomer = true
-    val previousPurchases = 0.0
+    val isRegularCustomer = false
+    val previousPurchases = 20000000.0
     val nowPurchase = 500.0
     val price = checkDiscount(nowPurchase, previousPurchases)
     val discountRegular = if(!isRegularCustomer) 0.0 else price * 0.01
@@ -16,7 +16,7 @@ fun checkDiscount(nowPurchase: Double, previousPurchases: Double): Double {
     return when (previousPurchases) {
         in 0.0..1000.0 -> nowPurchase
         in 1001.0..10000.0 -> nowPurchase - 100
-        else -> nowPurchase * 0.05
+        else -> nowPurchase - nowPurchase * 0.05
     }
 }
 
